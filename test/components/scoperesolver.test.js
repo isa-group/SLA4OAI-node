a80ee129b93548bcb48e6e644bf7630e05d4e818
ahmed-negm <anegm@icinetic.com>
2016-06-16 14:36:22 +0200

    Add scopeResolver.oauthConfig

--- test/components/scoperesolver.test.js
+++ test/components/scoperesolver.test.js
@@ -6,12 +6,18 @@ describe('scopeResolver', function () {
     describe('#configure', function () {
         it('options parameter is parsed correctly', function () {
             var options = {
-                oauthProvider: 'Google'
+                oauthProvider: 'Google',
+                oauthConfig: {
+                    clientId: '6076-d1m.apps.googleusercontent.com',
+                    clientSecret: '1lWx9DDDaPo9kxF4yu6t_loJ',
+                    callbackURL: 'https://app.myservice.com/google/callback'
+                }
             };
 
             slaManager.scopeResolver.configure(options);
 
             expect(slaManager.scopeResolver._oauthProvider).to.equal(options.oauthProvider);
+            expect(slaManager.scopeResolver._oauthConfig).to.equal(options.oauthConfig);
         });
     });
 });
