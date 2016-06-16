79236c38c9ef2280dc0da145f5484d7a1f152e7a
ahmed-negm <anegm@icinetic.com>
2016-06-16 17:16:47 +0200

    Add Facebook provider

--- test/sampleServer/server.js
+++ test/sampleServer/server.js
@@ -23,7 +23,7 @@ var monitorConnection = {
 };
 
 var scopeResolverOptions = {
-    oauthProvider: 'google',
+    oauthProvider: 'facebook',
     config: {
         google: {
             clientId: '323051689948-tcmek8pmu3jmof2clamm01imq11vc8i5.apps.googleusercontent.com',
