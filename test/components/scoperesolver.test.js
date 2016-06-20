ecfacc3f66eaac60ba49ad0d2d87ba597c3d8a38
ahmed-negm <anegm@icinetic.com>
2016-06-20 16:31:26 +0200

    ScopeResolver tests

--- test/components/scoperesolver.test.js
+++ test/components/scoperesolver.test.js
@@ -1,9 +1,10 @@
 /* global describe, it, expect */
-
-var slaManager = require('../../lib');
+var chai = require('chai');
+var scopeResolver = require('../../lib').scopeResolver;
+var SlaError = require('../../lib/errors/slaerror');
 
 describe('scopeResolver', function () {
-    describe('#configure', function () {
+    describe('configure', function () {
         it('options parameter is parsed correctly', function () {
             var options = {
                 oauthProvider: 'google',
@@ -16,10 +17,165 @@ describe('scopeResolver', function () {
                 }
             };
 
-            slaManager.scopeResolver.configure(options);
+            scopeResolver.configure(options);
 
-            expect(slaManager.scopeResolver._oauthProvider).to.equal(options.oauthProvider);
-            expect(slaManager.scopeResolver._config).to.equal(options.config);
+            expect(scopeResolver._oauthProvider).to.equal(options.oauthProvider);
+            expect(scopeResolver._config).to.equal(options.config);
+        });
+    });
+    describe('middleware Error', function () {
+        var error;
+        before(function (done) {
+            chai.connect.use(scopeResolver._middleware)
+                .req(function (req) {
+                    req.query = {};
+                })
+                .next(function (err) {
+                    error = err;
+                    done();
+                })
+                .dispatch();
+        });
+        it('When missing apikey and Bearer token', function () {
+            expect(error).to.be.an.instanceOf(Error);
+            expect(error.name).to.equal('SlaError');
+            expect(error.status).to.equal(401);
+            expect(error.message).to.equal('You need to pass apikey or Bearer token.');
+        });
+    });
+    describe('middleware with apikey', function () {
+        var queryName, queryValue, request, error;
+        var apikey = 'NbfiS7wjwjwb=';
+        var tenantsResult = {
+            sla: 'pro_petstore_YMkddfs',
+            scope: {
+                account: 'ahmed@icinetic.com',
+                tenant: 'icinetic'
+            },
+            requestedPayload: {
+                reportType: '/type'
+            }
+        };
+
+
+        before(function (done) {
+            scopeResolver.getAccountScope = function (paramName, paramValue, callback) {
+                queryName = paramName;
+                queryValue = paramValue;
+                callback(null, tenantsResult);
+            };
+            chai.connect.use(scopeResolver._middleware)
+                .req(function (req) {
+                    req.sla = {};
+                    req.query = { apikey: apikey };
+                    request = req;
+                })
+                .next(function (err) {
+                    error = err;
+                    done();
+                })
+                .dispatch();
+        });
+
+        it('should not error', function () {
+            expect(error).to.be.undefined;
+        });
+        it('request query should parsed correctly', function () {
+            expect(queryName).to.equal('apikey');
+            expect(queryValue).to.equal(apikey);
+        });
+        it('should set correct request.sla', function () {
+            expect(request.sla).to.be.an('Object');
+            expect(request.sla.agreement).to.equal(tenantsResult.sla);
+            expect(request.sla.scope).to.equal(tenantsResult.scope);
+            expect(request.sla.requestedPayload).to.equal(tenantsResult.requestedPayload);
+        });
+    });
+    describe('middleware with bearer token', function () {
+        var queryName2, queryValue2, request2, error2;
+        var email = 'negmsoftware@gmail.com';
+        var actualToken = 'ya29.CjUGA6l63n4LYBPxDnxATm05S8Lp9LM-o7me5n0jwoTpEVTmHgFIC8FsVBYK3bSu8TkvmVob6w';
+        var tenantsResult2 = {
+            sla: 'pro_petstore_YMkddfs',
+            scope: {
+                account: email,
+                tenant: 'icinetic'
+            },
+            requestedPayload: {
+                reportType: '/type'
+            }
+        };
+        before(function (done) {
+            scopeResolver.getAccountName = function (token, callback) {
+                if (token === actualToken) {
+                    callback(null, email);
+                }
+                else {
+                    return callback(new SlaError('Invalid token'));
+                }
+
+            };
+            scopeResolver.getAccountScope = function (paramName, paramValue, callback) {
+                queryName2 = paramName;
+                queryValue2 = paramValue;
+                callback(null, tenantsResult2);
+            };
+            done();
+        });
+
+        describe('Invalid token', function () {
+            before(function (done) {
+                chai.connect.use(scopeResolver._middleware)
+                    .req(function (req) {
+                        req.sla = {};
+                        req.query = {};
+                        req.headers = {
+                            authorization: 'Bearer ' + 'xx-invalid token-xx'
+                        };
+                        request2 = req;
+                    })
+                    .next(function (err) {
+                        error2 = err;
+                        done();
+                    })
+                    .dispatch();
+            });
+            it('Raise SlaError', function () {
+                expect(error2).to.be.an.instanceOf(Error);
+                expect(error2.name).to.equal('SlaError');
+                expect(error2.message).to.equal('Invalid token');
+            });
+        });
+        describe('Valid token', function () {
+            before(function (done) {
+                chai.connect.use(scopeResolver._middleware)
+                    .req(function (req) {
+                        req.sla = {};
+                        req.query = {};
+                        req.headers = {
+                            authorization: 'Bearer ' + actualToken
+                        };
+                        request2 = req;
+                    })
+                    .next(function (err) {
+                        error2 = err;
+                        done();
+                    })
+                    .dispatch();
+            });
+            it('should not error', function () {
+                expect(error2).to.be.undefined;
+            });
+            it('request query should parsed correctly', function () {
+                expect(queryName2).to.equal('account');
+                expect(queryValue2).to.equal(email);
+            });
+            it('should set correct request.sla', function () {
+                expect(request2.sla).to.be.an('Object');
+                expect(request2.sla.agreement).to.equal(tenantsResult2.sla);
+                expect(request2.sla.scope).to.equal(tenantsResult2.scope);
+                expect(request2.sla.requestedPayload).to.equal(tenantsResult2.requestedPayload);
+            });
         });
     });
 });
