fb29a7d3d5569de7d19c9a8b11ba1be9470cd143
ahmed-negm <anegm@icinetic.com>
2016-06-20 17:23:28 +0200

    Bouncer tests

--- test/components/bouncer.test.js
+++ test/components/bouncer.test.js
@@ -1,6 +1,6 @@
 /* global describe, it, expect */
-
-var slaManager = require('../../lib');
+var chai = require('chai');
+var bouncer = require('../../lib').bouncer;
 
 describe('bouncer', function () {
     describe('#configure', function () {
@@ -9,9 +9,82 @@ describe('bouncer', function () {
                 environment: 'qa'
             };
 
-            slaManager.bouncer.configure(options);
+            bouncer.configure(options);
 
-            expect(slaManager.bouncer._environment).to.equal(options.environment);
+            expect(bouncer._environment).to.equal(options.environment);
+        });
+    });
+
+    describe('middleware', function () {
+        var error;
+        var requestBody;
+        var environment = 'qa';
+        var sla = {
+            agreement: 'pro_petstore_YMkddfs',
+            ts: "2016-01-12T12:57:37.345Z",
+            resource: "/pets",
+            method: "POST",
+            scope: {
+                account: 'ahmed@icinetic.com',
+                tenant: 'icinetic'
+            },
+            requestedPayload: {
+                reportType: '/type'
+            }
+        };
+        before(function (done) {
+            bouncer._environment = environment;
+            bouncer.check = function (body, callback) {
+                requestBody = body;
+                callback(null, { accept: body.sla === 'pro_petstore_YMkddfs' });
+            };
+            done();
+        });
+        describe('"/check" api call', function () {
+            before(function (done) {
+                chai.connect.use(bouncer._middleware)
+                    .req(function (req) {
+                        req.query = {};
+                        req.sla = sla;
+                    })
+                    .next(function (err) {
+                        error = err;
+                        done();
+                    })
+                    .dispatch();
+            });
+            it('should not error', function () {
+                expect(error).to.be.undefined;
+            });
+            it('Valid request body', function () {
+                expect(requestBody.sla).to.equal(sla.agreement);
+                expect(requestBody.ts).to.equal(sla.ts);
+                expect(requestBody.resource).to.equal(sla.resource);
+                expect(requestBody.method).to.equal(sla.method);
+                expect(requestBody.scope).to.equal(sla.scope);
+                expect(requestBody.environment).to.equal(environment);
+            });
+        });
+        describe('Not accepted', function () {
+            before(function (done) {
+                chai.connect.use(bouncer._middleware)
+                    .req(function (req) {
+                        req.query = {};
+                        req.sla = sla;
+                        req.sla.agreement = 'xx-Invalid Agreement-xx';
+                    })
+                    .next(function (err) {
+                        error = err;
+                        done();
+                    })
+                    .dispatch();
+            });
+            it('error', function () {
+                expect(error).to.be.an.instanceOf(Error);
+                expect(error.name).to.equal('SlaError');
+                expect(error.status).to.equal(403);
+                expect(error.message.accept).to.equal(false);
+            });
         });
     });
 });
