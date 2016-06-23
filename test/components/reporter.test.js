d52b99802d728f5858616bd68fefac99917c78e1
ahmed-negm <anegm@icinetic.com>
2016-06-24 00:35:57 +0200

    Reporter skeleton

--- test/components/reporter.test.js
+++ test/components/reporter.test.js
@@ -9,7 +9,6 @@ describe('reporter', function () {
                 autoReport: true,
                 aggregate: true,
                 aggregationPeriod: 100,
-                autoPopulatedMetrics: ['ResponseTime', 'RequestHeaders'],
                 cluster: 'cl1.acme.com',
                 environment: 'qa'
             };
@@ -19,7 +18,6 @@ describe('reporter', function () {
             expect(slaManager.reporter._autoReport).to.equal(options.autoReport);
             expect(slaManager.reporter._aggregate).to.equal(options.aggregate);
             expect(slaManager.reporter._aggregationPeriod).to.equal(options.aggregationPeriod);
-            expect(slaManager.reporter._autoPopulatedMetrics).to.equal(options.autoPopulatedMetrics);
             expect(slaManager.reporter._cluster).to.equal(options.cluster);
             expect(slaManager.reporter._environment).to.equal(options.environment);
         });
